c80463f2f91bdf0451e0f6ccfc2d872f54610e18
Mufique Patwary <mdmusfiquepatwary@gmail.com>
2026-04-25 23:42:44 +0600

    ..

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,15 @@
 
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { env } from "prisma/config";
+import type { PrismaConfig } from "prisma";
 
 
-export default defineConfig({
+export default {
   schema: "prisma/schema",
   migrations: {
-    path: "prisma/migrations",
+    path: "prisma/migrations"
   },
   datasource: {
     url: env("DATABASE_URL"),
   },
-});
+} satisfies PrismaConfig;
